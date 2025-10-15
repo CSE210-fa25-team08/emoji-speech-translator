e10fa2741249738211b0696bd9db2f70b7705202
Sai Chaparala <cskrishna9306@gmail.com>
2025-10-15 12:32:56 -0700

    Reorganized directory structure to house all backend files under the src/backend/ subdirectory

--- src/Translation.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-class Translation(BaseModel):
-    """
-    Pydantic model to represent a translation from emoji to speech or vice-versa.
-    """
-    emoji: str
-    speech: str
